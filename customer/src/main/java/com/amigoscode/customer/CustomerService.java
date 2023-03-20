2164502076070f5ca42b9edf75b0fbaf6a97488b
D-Arturo <arturomf88@gmail.com>
2023-03-20 14:09:35 +0100

    feat: final setup of DB behaviour, repository and service

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.amigoscode.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -12,6 +12,7 @@ public record CustomerService() {
                 .build();
         // todo: check if email is valid
         // todo: check if email is not taken
-        // todo: store customer in db
+        //store customer in db
+        customerRepository.save(customer);
     }
 }
